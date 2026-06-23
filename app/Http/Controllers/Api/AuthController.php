a3fe39f7c11f0ee3cc89eb75f68267c4133a6dc5
Andrade . <j0s3.90jr@gmail.com>
2026-06-24 00:43:04 +0200

    correcciones

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class AuthController extends Controller
 {
